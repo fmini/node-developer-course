204d520d46afd085f72823640961c72fac5be467
Frank Minichiello <9028036+fmini@users.noreply.github.com>
2020-02-16 11:22:18 -0700

    asynchronous-and-callbacks-Initial-weather-app

---
+++ node-course/weather-app/courseNotes.js
@@ -0,0 +1,7 @@
+const fs = require('fs')
+
+fs.writeFileSync('courseNotes.txt',"Many times, you will want to run code asynchronously so your program doesn't pause.\n")
+fs.appendFileSync('courseNotes.txt',"This is especially true when your program needs to get data from a database or maybe an api or http request.\n")
+fs.appendFileSync('courseNotes.txt',"\n")
+fs.appendFileSync('courseNotes.txt',"\n")
+fs.appendFileSync('courseNotes.txt',"\n")
